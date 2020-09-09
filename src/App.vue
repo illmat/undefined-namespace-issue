893d69f239d86ac83900bc8427a73363240ebd32
Paul Knobloch <paul.knobloch@matelso.de>
2020-09-09 08:39:36 +0200

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Demo</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class App extends Vue {
+  foo: stripe.Token | null = null;
+}
+</script>
+
+<style>
+</style>
